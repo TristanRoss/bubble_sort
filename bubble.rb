4dbcde3651487f65843a55ce853818ab7c1a06c6
Tristan Ross <tgr1998@live.com>
2019-06-12 15:00:20 -0700

    Finish block method

--- bubble.rb
+++ bubble.rb
@@ -14,7 +14,7 @@ end
 def bubble_sort_by(arr)
     for i in 0..arr.length - 1
         for j in 0..i
-            if arr[i] > arr[j]
+            if yield(arr[j], arr[i]) >= 1
                 temp = arr[i]
                 arr[i] = arr[j]
                 arr[j] = temp
@@ -25,6 +25,4 @@ def bubble_sort_by(arr)
 end
 
 #puts bubble_sort([3, 2, 1, 800, 755])
-puts bubble_sort_by(["hi","hello","hey"]) do |left,right| 
-    left.length - right.length
-end
+puts bubble_sort_by(["hi","hello","hey"]) {|left,right| left.length - right.length }
