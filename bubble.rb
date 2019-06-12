55a77768bd5185c223c8ae40a30a1d11ce2736a1
Tristan Ross <tgr1998@live.com>
2019-06-12 13:54:19 -0700

    Add bubble sort with block method

--- bubble.rb
+++ bubble.rb
@@ -11,4 +11,20 @@ def bubble_sort(arr)
     return arr
 end
 
-puts bubble_sort([3, 2, 1, 800, 755])
+def bubble_sort_by(arr)
+    for i in 0..arr.length - 1
+        for j in 0..i
+            if arr[i] > arr[j]
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            end
+        end
+    end
+    return arr
+end
+
+#puts bubble_sort([3, 2, 1, 800, 755])
+puts bubble_sort_by(["hi","hello","hey"]) do |left,right| 
+    left.length - right.length
+end
